a5419b36dac73d0151894b10a5e3e51dfb2b8066
varshini <ar.varshini@gmail.com>
2020-04-19 21:44:45 -0400

    currency module

--- src/app/obtain-currency/obtain-currency.component.ts
+++ src/app/obtain-currency/obtain-currency.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataServiceService } from '../data-service.service';
+import { Observable } from 'rxjs';
+import { Router, RouterModule,ActivatedRoute } from '@angular/router';
+import { FormGroup,  FormControl,  Validators, NgForm } from '@angular/forms';
+import {AngularFireDatabase,AngularFireObject,AngularFireList} from '@angular/fire/database'
 @Component({
   selector: 'app-obtain-currency',
   templateUrl: './obtain-currency.component.html',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObtainCurrencyComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+//   datas$
+//   currencyForm: FormGroup;
+//   currency="";
+ 
+  constructor(private data: DataServiceService,private router: Router) { 
+    
+   
+//    this.data.getCurrencyDetail().subscribe(
+//     data => this.datas$ = data 
+//   );
+  
   }
+  ngOnInit(){
+   
+    }
+  
+  onSubmit(){
 
+  }
+  
+  
+  
 }
+
